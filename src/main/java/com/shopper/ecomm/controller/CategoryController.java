ea16f736893c15e9796d1c8c3ab98a6247d69b35
mmoazam <14061746+mmoazam@users.noreply.github.com>
2024-09-03 17:40:37 +0100

    wire up CategoryController

---
+++ src/main/java/com/shopper/ecomm/controller/CategoryController.java
@@ -0,0 +1,20 @@
+package com.shopper.ecomm.controller;
+
+import com.shopper.ecomm.model.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CategoryController {
+
+    private List<Category> categories = new ArrayList<>();
+
+    @GetMapping("/api/public/categories")
+    public List<Category> getAllCategories(){
+        return categories;
+    }
+
+}
