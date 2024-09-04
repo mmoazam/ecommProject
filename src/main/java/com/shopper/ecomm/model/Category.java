1ddaec0856631acb0e719222fb32c29b7c9bfd85
mmoazam <mmoazam@users.noreply.github.com>
2024-09-04 17:46:36 +0100

    refactor ID

--- src/main/java/com/shopper/ecomm/model/Category.java
+++ src/main/java/com/shopper/ecomm/model/Category.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 public class Category {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
 
     private String categoryName;
