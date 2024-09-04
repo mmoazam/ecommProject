b71f93e7ed26a6e34a2aeb4f08cc4f3d49b201eb
mmoazam <mmoazam@users.noreply.github.com>
2024-09-04 13:40:00 +0100

    add

---
+++ src/main/java/com/shopper/ecomm/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.shopper.ecomm.repositories;
+
+import com.shopper.ecomm.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
