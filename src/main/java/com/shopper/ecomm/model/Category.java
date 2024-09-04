4d6d9850e0728178892a2ac31acaae1cc8c7cdf7
mmoazam <mmoazam@users.noreply.github.com>
2024-09-04 17:46:04 +0100

    refactor with lombok

--- src/main/java/com/shopper/ecomm/model/Category.java
+++ src/main/java/com/shopper/ecomm/model/Category.java
@@ -1,9 +1,15 @@
 package com.shopper.ecomm.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Entity
-@Table(name = "categories")
+@Entity(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
 
     @Id
@@ -12,26 +18,4 @@ public class Category {
 
     private String categoryName;
 
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-    public Category(){}
 }
