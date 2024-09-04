fdb12cf46884b2371d8a45c4cd88449321a16491
mmoazam <mmoazam@users.noreply.github.com>
2024-09-04 11:18:06 +0100

    wire up delete endpoint with ResponseEntity

--- src/main/java/com/shopper/ecomm/controller/CategoryController.java
+++ src/main/java/com/shopper/ecomm/controller/CategoryController.java
@@ -2,7 +2,10 @@ package com.shopper.ecomm.controller;
 
 import com.shopper.ecomm.model.Category;
 import com.shopper.ecomm.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,21 +22,25 @@ public class CategoryController {
     }
 
     @GetMapping("/api/public/categories")
-    public List<Category> getAllCategories(){
+    public List<Category> getAllCategories() {
         return categoryService.getAllCategories();
     }
 
     @PostMapping("/api/public/categories")
-    public String createCategory(@RequestBody Category category){
+    public String createCategory(@RequestBody Category category) {
         category.setCategoryId(nextId++);
         categoryService.createCategory(category);
         return "category created";
     }
 
     @DeleteMapping("/api/public/categories/{categoryId}")
-    public String deleteCategory(@PathVariable Long categoryId){
-        String status = categoryService.deleteCategory(categoryId);
-        return status;
+    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
+        try {
+            String status = categoryService.deleteCategory(categoryId);
+            return new ResponseEntity<>(status, HttpStatus.OK);
+        } catch (ResponseStatusException e) {
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }
     }
 
 }
--- src/main/java/com/shopper/ecomm/service/CategoryServiceImpl.java
+++ src/main/java/com/shopper/ecomm/service/CategoryServiceImpl.java
@@ -1,7 +1,10 @@
 package com.shopper.ecomm.service;
 
 import com.shopper.ecomm.model.Category;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,11 +27,7 @@ public class CategoryServiceImpl implements CategoryService {
     public String deleteCategory(Long categoryId) {
         Category category = categories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst().orElse(null);
-
-        if (null == category) {
-            return "category not found.";
-        }
+                .findFirst().orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
 
         categories.remove(category);
 
