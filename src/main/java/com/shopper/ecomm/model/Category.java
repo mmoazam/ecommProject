28a0a8cc00062bf6060dae6e6a4d3fab42e307f9
mmoazam <mmoazam@users.noreply.github.com>
2024-09-04 13:29:52 +0100

    annotate class

--- src/main/java/com/shopper/ecomm/model/Category.java
+++ src/main/java/com/shopper/ecomm/model/Category.java
@@ -1,8 +1,16 @@
 package com.shopper.ecomm.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categories")
 public class Category {
 
+    @Id
     private Long categoryId;
+
     private String categoryName;
 
     public Long getCategoryId() {
