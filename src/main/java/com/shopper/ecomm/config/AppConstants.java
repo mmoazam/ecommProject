b324b02cd488d7be410b5ff9555b5a4439102c92
mmoazam <mmoazam@users.noreply.github.com>
2024-09-06 10:23:12 +0100

    impl: sorting for getAllCategories endpoint

--- src/main/java/com/shopper/ecomm/config/AppConstants.java
+++ src/main/java/com/shopper/ecomm/config/AppConstants.java
@@ -3,4 +3,6 @@ package com.shopper.ecomm.config;
 public class AppConstants {
     public final static String PAGE_NUMBER = "0";
     public final static String PAGE_SIZE = "50";
+    public static final String SORT_BY = "categoryId";
+    public static final String SORT_ORDER = "asc";
 }
--- src/main/java/com/shopper/ecomm/controller/CategoryController.java
+++ src/main/java/com/shopper/ecomm/controller/CategoryController.java
@@ -27,10 +27,12 @@ public class CategoryController {
     //@GetMapping("/api/public/categories")
     @RequestMapping(value = "/public/categories", method = RequestMethod.GET)
     public ResponseEntity<CategoryResponse> getAllCategories(
-            @RequestParam (defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
-            @RequestParam (defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize
+            @RequestParam (name = "pageNumber",defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+            @RequestParam (name = "pageSize",defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+            @RequestParam (name = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
+            @RequestParam (name = "sortOrder", defaultValue = AppConstants.SORT_ORDER, required = false) String sortOrder
     ) {
-        CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber, pageSize);
+        CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber, pageSize, sortBy, sortOrder);
         return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
 
