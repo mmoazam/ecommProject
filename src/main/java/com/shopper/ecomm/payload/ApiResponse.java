0b2127a816a1f354b518a205f1d3e3b003711a51
mmoazam <mmoazam@users.noreply.github.com>
2024-09-06 10:26:35 +0100

    impl: Class for standard output of Api messages

---
+++ src/main/java/com/shopper/ecomm/payload/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.shopper.ecomm.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse {
+    private String message;
+    private boolean success;
+}
