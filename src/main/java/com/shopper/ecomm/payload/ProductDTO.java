d91d4fdee318e3d405ca8a79d6fb7e02eee130b9
mmoazam <mmoazam@users.noreply.github.com>
2024-09-09 10:07:11 +0100

    impl: ProductDTO and Response

--- src/main/java/com/shopper/ecomm/payload/ProductDTO.java
+++ src/main/java/com/shopper/ecomm/payload/ProductDTO.java
@@ -1,4 +1,11 @@
 package com.shopper.ecomm.payload;
 
 public class ProductDTO {
+    private Long productId;
+    private String productName;
+    private String image;
+    private Integer quantity;
+    private double price;
+    private double discount;
+    private double specialPrice;
 }
