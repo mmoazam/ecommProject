e8704b54c2e0a24484fa75637cbcfb60584fd508
Mister <14061746+mmoazam@users.noreply.github.com>
2024-09-16 22:22:00 +0100

    setup class

--- src/main/java/com/shopper/ecomm/model/Address.java
+++ src/main/java/com/shopper/ecomm/model/Address.java
@@ -1,7 +1,14 @@
 package com.shopper.ecomm.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
 }
--- src/main/java/com/shopper/ecomm/model/EcommUser.java
+++ src/main/java/com/shopper/ecomm/model/EcommUser.java
@@ -1,7 +1,35 @@
 package com.shopper.ecomm.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor
+@Data
+@Table(name = "users")
 public class EcommUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    public EcommUser(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 }
