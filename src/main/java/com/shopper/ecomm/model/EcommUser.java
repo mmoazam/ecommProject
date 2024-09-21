cbf700900a9cedb2f214fe25c5e8e0e348de019c
Mister <14061746+mmoazam@users.noreply.github.com>
2024-09-21 10:16:52 +0100

    setup relationships

--- src/main/java/com/shopper/ecomm/model/EcommUser.java
+++ src/main/java/com/shopper/ecomm/model/EcommUser.java
@@ -4,12 +4,21 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @NoArgsConstructor
 @Data
-@Table(name = "users")
+@Table(name = "users",
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+})
 public class EcommUser {
 
     @Id
@@ -18,13 +27,16 @@ public class EcommUser {
     private Long userId;
 
     @NotBlank
+    @Column(name = "username")
     private String username;
 
     @NotBlank
     @Email
+    @Column(name = "email")
     private String email;
 
     @NotBlank
+    @Column(name = "password")
     private String password;
 
     public EcommUser(String username, String email, String password) {
@@ -32,4 +44,14 @@ public class EcommUser {
         this.email = email;
         this.password = password;
     }
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+            fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id"))
+    @Setter
+    @Getter
+    @Column(name = "role_id")
+    private Set<Role> roleSet = new HashSet<>();
 }
